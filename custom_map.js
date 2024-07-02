27f752a4a309d0eb8173b0f10141b88f86be34a8
rajanjha66 <170436550+rajanjha66@users.noreply.github.com>
2024-07-02 19:00:16 +0530

    Update custom_map.js

--- custom_map.js
+++ custom_map.js
@@ -1,63 +1,82 @@
+// Define the visualization object
 looker.plugins.visualizations.add({
-  id: "heat-density-map",
-  label: "Heat Density Map",
+  id: "custom_map",
+  label: "Custom Map with Multiple Measures",
   options: {
-    color1: {
-      type: "string",
-      label: "Measure 1 Color",
-      display: "color",
-      default: "#ff0000"
+    color_range: {
+      type: "array",
+      label: "Color Range",
+      default: ["#f00", "#0f0", "#00f"],
     },
-    color2: {
+    measure_one_color: {
       type: "string",
-      label: "Measure 2 Color",
-      display: "color",
-      default: "#0000ff"
-    }
+      label: "Measure One Color",
+      default: "#f00",
+    },
+    measure_two_color: {
+      type: "string",
+      label: "Measure Two Color",
+      default: "#0f0",
+    },
   },
-  create: function(element, config) {
-    element.innerHTML = `
-      <style>
-        #map { height: 100%; }
-      </style>
-      <div id="map"></div>
+  create: function (element, config) {
+    element.innerHTML = "<div id='map'></div>";
+    var css = `
+      #map {
+        width: 100%;
+        height: 100%;
+      }
     `;
-
-    const leafletCss = document.createElement("link");
-    leafletCss.rel = "stylesheet";
-    leafletCss.href = "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css";
-    document.head.appendChild(leafletCss);
-
-    const leafletScript = document.createElement("script");
-    leafletScript.src = "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js";
-    leafletScript.onload = () => {
-      this.map = L.map(element.querySelector("#map")).setView([0, 0], 2);
-
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19
-      }).addTo(this.map);
-
-      this.heatLayer1 = L.heatLayer([], { radius: 25, blur: 15, maxZoom: 17, gradient: { 0.4: config.color1 } });
-      this.heatLayer2 = L.heatLayer([], { radius: 25, blur: 15, maxZoom: 17, gradient: { 0.4: config.color2 } });
-
-      this.map.addLayer(this.heatLayer1);
-      this.map.addLayer(this.heatLayer2);
-    };
-    document.head.appendChild(leafletScript);
+    var style = document.createElement("style");
+    style.type = "text/css";
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+    document.head.appendChild(style);
   },
-  updateAsync: function(data, element, config, queryResponse, details, done) {
-    if (!this.map) return;
+  update: function (data, element, config, queryResponse) {
+    var mapElement = document.getElementById("map");
 
-    const locationField = queryResponse.fields.dimension_like[0].name;
-    const measure1 = queryResponse.fields.measure_like[0].name;
-    const measure2 = queryResponse.fields.measure_like[1].name;
+    if (!mapElement) {
+      this.create(element, config);
+      mapElement = document.getElementById("map");
+    }
 
-    const heatData1 = data.map(row => [row[locationField].value[0], row[locationField].value[1], row[measure1].value]);
-    const heatData2 = data.map(row => [row[locationField].value[0], row[locationField].value[1], row[measure2].value]);
+    // Clear previous map if any
+    while (mapElement.firstChild) {
+      mapElement.removeChild(mapElement.firstChild);
+    }
 
-    this.heatLayer1.setLatLngs(heatData1);
-    this.heatLayer2.setLatLngs(heatData2);
+    // Prepare map data
+    var locations = data.map(function (row) {
+      return {
+        location: row[queryResponse.fields.dimension_like[0].name].value,
+        measure_one: row[queryResponse.fields.measure_like[0].name].value,
+        measure_two: row[queryResponse.fields.measure_like[1].name].value,
+      };
+    });
 
-    done();
+    // Initialize the map (using Leaflet.js as an example)
+    var map = L.map(mapElement).setView([37.8, -96], 4);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Define color scales for measures
+    var measureOneColor = config.measure_one_color;
+    var measureTwoColor = config.measure_two_color;
+
+    locations.forEach(function (loc) {
+      var marker = L.circleMarker([loc.location.lat, loc.location.lng], {
+        color: measureOneColor,
+        fillColor: measureTwoColor,
+        fillOpacity: 0.5,
+        radius: Math.sqrt(loc.measure_one + loc.measure_two) * 2
+      }).addTo(map);
+      
+      marker.bindPopup("<b>Location:</b> " + loc.location.name + "<br><b>Measure One:</b> " + loc.measure_one + "<br><b>Measure Two:</b> " + loc.measure_two);
+    });
   }
 });
